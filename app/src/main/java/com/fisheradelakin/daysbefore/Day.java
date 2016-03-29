0674588e5a2a7ece5ecfaa59c151e0830dd86737
Fisher Adelakin <temidayoadelakin@gmail.com>
2016-03-29 12:08:46 -0400

    initial commit

---
+++ app/src/main/java/com/fisheradelakin/daysbefore/Day.java
@@ -0,0 +1,39 @@
+package com.fisheradelakin.daysbefore;
+
+import java.util.Date;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by temidayo on 3/29/16.
+ */
+public class Day extends RealmObject {
+
+    private Date date;
+    private String occassion;
+    private String color;
+    
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getOccassion() {
+        return occassion;
+    }
+
+    public void setOccassion(String occassion) {
+        this.occassion = occassion;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
