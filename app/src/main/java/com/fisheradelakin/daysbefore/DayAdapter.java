f4222f65dcd46eccf694c005665c54b6949a9f64
Fisher Adelakin <temidayoadelakin@gmail.com>
2016-03-29 15:39:19 -0400

    todos for later

---
+++ app/src/main/java/com/fisheradelakin/daysbefore/DayAdapter.java
@@ -0,0 +1,14 @@
+package com.fisheradelakin.daysbefore;
+
+import android.content.Context;
+
+import io.realm.RealmResults;
+
+/**
+ * Created by temidayo on 3/29/16.
+ */
+public class DayAdapter {
+    public DayAdapter(Context context, RealmResults<Day> days) {
+
+    }
+}
--- app/src/main/java/com/fisheradelakin/daysbefore/MainActivity.java
+++ app/src/main/java/com/fisheradelakin/daysbefore/MainActivity.java
@@ -4,29 +4,54 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.Bind;
 import butterknife.OnClick;
+import io.realm.Realm;
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
+
+    // TODO: Write code to update Day in case user wants to change date
+    // TODO: > Day day = realm.where(Day.class).equals("timestamp", originalDay.getTimestamp()).findFirst();
+    // TODO: > realm.beginTransaction();
+    // TODO: > set the values of the updated day
+    // TODO: > realm.commitTransaction();
+
+    @Bind(R.id.fab) FloatingActionButton fab;
+    @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.days_rv) RecyclerView daysRV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Realm realm = Realm.getDefaultInstance();
+        RealmQuery<Day> dayRealmQuery = realm.where(Day.class);
+        RealmResults<Day> days = dayRealmQuery.findAll();
+
+        DayAdapter adapter = new DayAdapter(this, days);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
     }
 
     @OnClick(R.id.fab)
     public void addDay() {
 
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
